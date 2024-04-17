259ab1ef69a89eb166fdfc25696112f9501b5813
Kiran Babu <kiranbabu3355@gmail.com>
2024-04-17 12:48:15 +0530

    Remove empty line in App component

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,6 @@ import {View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center ">
-
       <StatusBar />
       <Link href="/Home">Go to home</Link>
     </View>
