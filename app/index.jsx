ff00d0a0b7c94d6e7852a9478f0dee9dd370d9ac
Kiran Babu <kiranbabu3355@gmail.com>
2024-04-17 12:42:31 +0530

    Refactor App component to remove unused code and update layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,11 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Image, Text, View } from 'react-native';
+import {View } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center ">
-        <View >
-            <Image className="h-10" source={require('../assets/home.png')} resizeMode='contain' />
-        </View>
+
       <StatusBar />
       <Link href="/Home">Go to home</Link>
     </View>
