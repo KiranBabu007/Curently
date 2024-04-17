c0245299a411957c6a5c21a19321baf385d0f7ed
Kiran Babu <kiranbabu3355@gmail.com>
2024-04-17 11:05:22 +0530

    Update text in App component to display "Success" instead of the previous message

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-red">
-      <Text className="text-3xl">Open up App.js to start working on your app!</Text>
+      <Text className="text-3xl">Success</Text>
       <StatusBar />
     </View>
   );
