1c052420d21d99c87b259da0d7dd34dbf81c9e39
Kiran Babu <kiranbabu3355@gmail.com>
2024-05-02 01:05:31 +0530

    Refactor styles and add energy consumption data display in Calendar.jsx

--- app/(tabs)/Calendar.jsx
+++ app/(tabs)/Calendar.jsx
@@ -3,6 +3,8 @@ import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { firestore } from '../../firebaseConfig';
 import { collection, getDocs, query, where } from 'firebase/firestore';
+import { LinearGradient } from 'expo-linear-gradient';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const MyCalendar = () => {
   const dbInstance = collection(firestore, 'values');
@@ -52,17 +54,29 @@ const MyCalendar = () => {
       </View>
 
       {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" style={styles.activityIndicator} />
+        <ActivityIndicator size="large" color="#00adf5" style={styles.activityIndicator} />
       ) : (
         <View style={styles.dataContainer}>
           {fetchedData ? (
-            <>
-              <Text>Data Retrieved for {selectedDate}:</Text>
-              <Text>Max Energy Retrieved: {Math.max(...fetchedData.map((item) => item.energy))}</Text>
-            </>
+            <LinearGradient
+            colors={['#e6f7ff', '#ffffff']} 
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.dataCardContainer}
+            >
+              <Text className="font-bold text-2sm " style={styles.dataCardTitle}>Energy Consumption Data</Text>
+               <Text className="font-bold text-2sm" style={styles.dataCardSubtitle}>for {selectedDate}</Text>
+              <View className="flex flex-row m-5 space-x-2" style={styles.maxEnergyContainer}>
+                <FontAwesome5 name="bolt" size={28} color="gold" />
+                <Text  className="font-extrabold text-xl " style={styles.maxEnergyText}>
+                  {Math.max(...fetchedData.map((item) => item.energy))} KwH
+                </Text>
+              </View>
+             
+            </LinearGradient>
           ) : (
-            <View style={styles.noDataCard}>
-              <Text style={styles.noDataText}>No results</Text>
+            <View style={styles.noDataContainer}>
+              <Text style={styles.noDataText}>Click on any date to show the energy consumption data.</Text>
             </View>
           )}
         </View>
@@ -91,7 +105,7 @@ const styles = StyleSheet.create({
   dataContainer: {
     backgroundColor: '#fff',
     borderRadius: 8,
-    padding: 16,
+    
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -100,6 +114,18 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 2,
+    width:300,
+  },
+  dataCardContainer: {
+    padding:16,
+    borderRadius: 8,
+    alignItems: 'center',
+    margin:0,
+  },
+  dataCardTitle:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
   },
   noDataCard: {
     backgroundColor: '#fff',
