45924990baa9bca81c765583233e90b6e8c6450a
Kiran Babu <kiranbabu3355@gmail.com>
2024-05-01 16:31:15 +0530

    Refactor appearance settings icon and label in Settings.jsx

--- app/(tabs)/Settings.jsx
+++ app/(tabs)/Settings.jsx
@@ -19,7 +19,7 @@ const SettingsScreen = () => {
       {settingsData.map((item, index) => (
         <TouchableOpacity onPress={()=>{  router.replace(`/Screens${item.href}`)}} key={index} style={styles.settingItem}>
           
-            <View  style={styles.settingItemContent}>
+            <View style={styles.settingItemContent}>
               <Ionicons name={item.icon} size={24} color="#333" />
               <Text style={styles.settingLabel}>{item.label}</Text>
               <Ionicons name="chevron-forward" size={20} color="#888" />
